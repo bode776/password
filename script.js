95a01604bb2485ca430775621144f384de781060
Mr.Robot <96988396+bode776@users.noreply.github.com>
2022-03-03 08:22:24 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let screen = document.getElementById("pass-output");
-let generateButton = document.getElementById("generate-btn");
+const screen = document.getElementById("pass-output");
+const generateButton = document.getElementById("generate-btn");
+let lengthSet = document.getElementById("pass-length").value;
 
 let textArray = [
     "a",
@@ -37,15 +38,25 @@ let textArray = [
     "9",
 ];
 
-function passGen() {
+function passGen(length) {
     let password = [];
-    for (let i = 0; i <= 10; i++) {
+    for (let i = 0; i < length; i++) {
         let randNum = Math.floor(Math.random() * textArray.length);
         password += textArray[randNum];
     }
     return password;
 }
-
+function getLength() {
+    lengthSet = document.getElementById("pass-length").value;
+    if (lengthSet === "") {
+        return 3;
+    }
+    if (lengthSet > 20) {
+        document.getElementById("pass-length").value = "20";
+        return 20;
+    }
+    return lengthSet;
+}
 function print() {
-    screen.textContent = passGen();
+    screen.textContent = passGen(getLength());
 }
